27dff86ec9055335a7c348b3f26086923e789ae8
Jeremy Zilar <jeremyzilar@gmail.com>
2015-11-22 21:56:41 -0500

    styles to the popup

--- worthreading-add.php
+++ worthreading-add.php
@@ -32,6 +32,9 @@ if ( !current_user_can( 'manage_options' ) ) {
 				<title>Add bookmark</title>
 				<link rel="icon" href="<?php echo plugins_url( 'favicon-add.png' , __FILE__ ); ?>">
 
+				<!-- Fonts: Lato / http://www.latofonts.com/ -->
+				<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
       </head>
 
       <body>
@@ -60,6 +63,9 @@ if ( !current_user_can( 'manage_options' ) ) {
 						// give positive user feedback
 						echo <<< EOF
 						<div id="head">
+							<div class="color color-dark"></div>
+							<div class="color color-med"></div>
+							<div class="color color-light"></div>
 							<h1>Worth Reading</h1>
 						</div>
 						<div class="msg">
@@ -88,6 +94,9 @@ EOF;
 
 					?>
 					<div id="head">
+						<div class="color color-dark"></div>
+						<div class="color color-med"></div>
+						<div class="color color-light"></div>
 						<h1>Worth Reading</h1>
 					</div>
 
@@ -99,17 +108,19 @@ EOF;
 						</div>
 						
 						<div class="bookmark-url">
-							<label for="link_url">URL:</label>
+							<label for="link_url">URL</label>
 							<input type="text" id="link_url" tabindex="2" name="link_url" value="<?php if (strlen($_GET['url']) > 0 ) { echo $_GET['url']; } ?>" />	
+							<small>e.g. http://nytimes.com</small>
 						</div>
 
 						<div class="bookmark-pub">
 							<label for="publications">Publication</label>
 							<input type="text" id="publications" tabindex="3" name="publications" />
+							<small>e.g. The New York Times</small>
 						</div>
 
 						<div class="bookmark-desc">
-							<label for="link_desc">Description:</label>
+							<label for="link_desc">Description</label>
 							<textarea type="text" id="link_desc" tabindex="4" name="link_desc" /><?php if (strlen($_GET['desc']) > 0 ) { echo $_GET['desc']; } ?></textarea>
 						</div>
 						
@@ -117,7 +128,8 @@ EOF;
 						
 						<div class="bookmark-via">
 							<label for="link_via">via:</label>
-							<input type="text" id="link_via" tabindex="5" name="link_via" />	
+							<input type="text" id="link_via" tabindex="5" name="link_via" />
+							<small>e.g. @jeremyzilar</small>
 						</div>
 						
 						<div>
